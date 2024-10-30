4c11317715b8831412f236c0c8fc9760e9d173a7
TuikartT8 <benilunga111@gmail.com>
2024-10-30 16:53:30 +0100

    Added prettier

--- www/navbar.jsx
+++ www/navbar.jsx
@@ -4,29 +4,42 @@ import {
   FontDecreaseRegular,
   TextFontRegular,
 } from "@fluentui/react-icons";
-import { Toolbar, ToolbarButton } from "@fluentui/react-components";
+import { makeStyles, Toolbar, ToolbarButton } from "@fluentui/react-components";
 
 
 const Logo = () => {
     return (
         <div>
+            <img src="https://placehold.co/64x64"/>
         </div>
     );
 }
 
-export const Navbar = (props) => (
-    <div>
-        <Logo/>
-        <Toolbar aria-label="Vertical Button" {...props}>
-            <ToolbarButton vertical appearance="primary" icon={<AddFilled />}>
-            Nouveau
-            </ToolbarButton>
-            <ToolbarButton vertical icon={<FontDecreaseRegular />}>
-            Historique
-            </ToolbarButton>
-            <ToolbarButton vertical icon={<TextFontRegular />}>
-            Configuration
-            </ToolbarButton>
-        </Toolbar>
-    </div>
-)
+const useClasses = makeStyles({
+    container: {
+        display: 'flex',
+        flexDirection: 'row',
+    }
+})
+
+export const Navbar = (props) => {
+    const styles = useClasses();
+
+    return (
+        <div className={styles.container}>
+            <Logo/>
+            <span className="flex-expand"></span>
+            <Toolbar aria-label="Vertical Button" {...props}>
+                <ToolbarButton vertical appearance="primary" icon={<AddFilled />}>
+                Nouveau
+                </ToolbarButton>
+                <ToolbarButton vertical icon={<FontDecreaseRegular />}>
+                Historique
+                </ToolbarButton>
+                <ToolbarButton vertical icon={<TextFontRegular />}>
+                Configuration
+                </ToolbarButton>
+            </Toolbar>
+        </div>
+    );
+}
