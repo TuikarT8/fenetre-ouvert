921f6019e6daba4b35e2f8ed5ac998c7c9083bcb
TuikartT8 <benilunga111@gmail.com>
2024-11-26 21:25:51 +0100

    File input for upload

--- www/features/inventory/inventory-toolbar.jsx
+++ www/features/inventory/inventory-toolbar.jsx
@@ -1,10 +1,15 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { Toolbar, ToolbarButton, Tooltip } from '@fluentui/react-components';
 import { GoodCreationDialog } from '../../common/dialogs/good-creation.dialog';
-import { AddFilled } from '@fluentui/react-icons';
+import { AddFilled, ArrowUploadFilled } from '@fluentui/react-icons';
 
 export const InventoryToolbar = () => {
 	const [isCreateGoodDialogOpen, setIsCreateGoodDialogOpen] = useState(false);
+	const fileInputRef = useRef(null);
+
+	function handleFileUploadClick() {
+		fileInputRef.current?.click();
+	}
 
 	const onCreateMenuOptionSelected = () => {
 		setIsCreateGoodDialogOpen(true);
@@ -17,17 +22,33 @@ export const InventoryToolbar = () => {
 					<ToolbarButton
 						vertical
 						onClick={onCreateMenuOptionSelected}
-						icon={<AddFilled />}
-					/>
+						icon={<AddFilled />}>
+						Ajouter
+					</ToolbarButton>
 				</Tooltip>
-				<GoodCreationDialog
-					title="Ajouter un bien"
-					open={isCreateGoodDialogOpen}
-					onClose={() => {
-						setIsCreateGoodDialogOpen(false);
-					}}
-				/>
+
+				<Tooltip
+					content={"Importer des biens à partir d'un fichier JSON, CSV ou XML"}
+					relationship="description">
+					<ToolbarButton
+						vertical
+						onClick={handleFileUploadClick}
+						icon={<ArrowUploadFilled />}>
+						Importer
+					</ToolbarButton>
+				</Tooltip>
 			</Toolbar>
+			<GoodCreationDialog
+				title="Ajouter un bien"
+				open={isCreateGoodDialogOpen}
+				onClose={() => {
+					setIsCreateGoodDialogOpen(false);
+				}}
+			/>
+			<input
+				ref={fileInputRef}
+				type="file"
+				style={{ visibility: 'invisible', width: 0, height: 0 }}></input>
 		</div>
 	);
 };
